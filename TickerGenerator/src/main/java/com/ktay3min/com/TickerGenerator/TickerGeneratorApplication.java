c55784d73442b33abccd714b28f64238f970c4c2
0ktay3min <oktay_emin@hotmail.com>
2021-01-02 09:15:57 -0600

    TicketGenerator directory added

---
+++ TickerGenerator/src/main/java/com/ktay3min/com/TickerGenerator/TickerGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.ktay3min.com.TickerGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickerGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickerGeneratorApplication.class, args);
+	}
+
+}
---
+++ TickerGenerator/src/test/java/com/ktay3min/com/TickerGenerator/TickerGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktay3min.com.TickerGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickerGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
